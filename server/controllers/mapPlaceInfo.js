fbf3a0b357e656cca0c140f4c06e9a65de1ddd75
ichang1 <isaac.chang717@gmail.com>
2021-03-23 00:43:59 -0400

    can get map data from disease.sh api

--- server/controllers/mapPlaceInfo.js
+++ server/controllers/mapPlaceInfo.js
@@ -1,10 +1,19 @@
 import locationsApi from "../data/locationsApi.js";
+import capitalizeSeparateWords from "..//capitalizeSeparateWords.js";
+import axios from "axios";
 
-export const getMapPlaceInfo = (req, res) => {
-  const place = req.params.place;
-  console.log(place);
-  console.log(locationsApi[place]);
-  const api = locationsApi[place].api;
-  const url = locationsApi[place].path;
-  res.send({ place, api, url });
+export const getMapPlaceInfo = async (req, res) => {
+  if (req.url === "/favicon.ico") {
+    res.writeHead(200, { "Content-Type": "image/x-icon" });
+    res.end();
+    console.log("favicon requested");
+    return;
+  } else {
+    const place = req.params.place;
+    const formattedPlace = capitalizeSeparateWords(place);
+    const { api, path } = locationsApi[formattedPlace];
+    const response = await axios.get(path);
+    const data = response.data;
+    res.send({ formattedPlace, api, path, data });
+  }
 };
