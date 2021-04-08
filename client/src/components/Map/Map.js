790fa4d64c083df6bdbdbe160e2cf8296436931b
ichang1 <isaac.chang717@gmail.com>
2021-04-08 01:36:45 -0400

    got covid data for each place

--- client/src/components/Map/Map.js
+++ client/src/components/Map/Map.js
@@ -1,17 +1,38 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import "./Map.css";
 import ReactMapGl, { Marker } from "react-map-gl";
 import { locations } from "../../data/locations";
+import { Markers } from "../Markers/Markers";
 
 const Map = () => {
-  const [viewport, setViewport] = useState({
+  const INITIAL_VIEWPORT = {
     latitude: 0,
     longitude: 0,
     width: "80vw",
     height: "80vh",
     zoom: 0,
-  });
-  const mapbox_style = "mapbox://styles/isaacc/ckmcxu2s609qb17rwla4bcixc";
+  };
+  const [viewport, setViewport] = useState(INITIAL_VIEWPORT);
+  const markers = useRef({});
+
+  // useEffect(() => {
+  //   const initialMarkers = {};
+  //   Object.entries(locations).forEach(([place, placeData]) => {
+  //     initialMarkers[`${place}`] = null;
+  //   });
+  //   markers.current = initialMarkers;
+  //   console.log(markers);
+  // }, []);
+
+  // useEffect(() => {
+  //   Object.keys(markers.current).map((placeName) => {
+  //     const placeArea = locations.data[placeName].area;
+  //     console.log(placeName, placeArea);
+  //   });
+  // }, [viewport]);
+
+  const mapbox_style =
+    "mapbox://styles/isaacc/ckmcxu2s609qb17rwla4bcixc?optimize=true";
   const getMarkerLocations = () => {};
   return (
     <div className="map-container">
@@ -21,30 +42,61 @@ const Map = () => {
         mapStyle={mapbox_style}
         onViewportChange={(viewport) => {
           setViewport(viewport);
+          console.log(viewport);
         }}
       >
-        {locations.geoData.map((place, idx) => (
-          <Marker
-            key={idx}
-            latitude={place.latitude}
-            longitude={place.longitude}
-          >
-            <button
-              className="marker-btn"
-              id={place.place}
-              onClick={(e) => {
-                e.preventDefault();
-                console.log(e.currentTarget.id);
-                console.log(viewport.zoom);
-              }}
-            >
-              <img src="/map-pin.svg" />
-            </button>
-          </Marker>
-        ))}
+        <Markers data={Object.entries(locations)} zoom={viewport.zoom} />
       </ReactMapGl>
+      <button
+        value="+"
+        onClick={(e) => {
+          e.preventDefault();
+          const newZoom = viewport.zoom + 1;
+          const max = viewport.maxZoom;
+          setViewport({
+            ...viewport,
+            zoom: newZoom >= max ? viewport.maxZoom : newZoom,
+          });
+        }}
+      >
+        +
+      </button>
+      <button
+        value="-"
+        onClick={(e) => {
+          e.preventDefault();
+          const newZoom = viewport.zoom - 1;
+          const min = viewport.minZoom;
+          setViewport({ ...viewport, zoom: newZoom <= min ? min : newZoom });
+        }}
+      >
+        -
+      </button>
     </div>
   );
 };
 
+// {
+//   Object.entries(locations).map(([place, placeData], idx) => (
+//     <Marker
+//       key={idx}
+//       latitude={placeData.latitude}
+//       longitude={placeData.longitude}
+//     >
+//       <button
+//         className="marker-btn"
+//         id={`${place}-${idx}`}
+//         onClick={(e) => {
+//           e.preventDefault();
+//           console.log(e.currentTarget.id);
+//           console.log({ place, ...placeData });
+//           console.log(viewport.zoom);
+//         }}
+//         ref={(el) => (markers.current[`${place}`] = el)}
+//       >
+//         <img src="/map-pin.svg" />
+//       </button>
+//     </Marker>
+//   ));
+// }
 export default Map;
---
+++ client/src/components/Markers/Markers.js
@@ -0,0 +1,83 @@
+import React, { useEffect, useRef } from "react";
+import { Marker } from "react-map-gl";
+import "./Markers.css";
+import axios from "axios";
+
+export const Markers = React.memo(({ data, zoom }) => {
+  // const markerData = data.map(([place, placeData]) => {
+  //   return { ...placeData, place };
+  // });
+
+  const markerRefs = useRef({});
+  const markerData = useRef({});
+
+  useEffect(() => {
+    const initialMarkerRefs = {};
+    data.forEach(([place, placeData]) => {
+      initialMarkerRefs[`${place}`] = { style: { display: "" } };
+    });
+    markerRefs.current = initialMarkerRefs;
+  }, []);
+
+  useEffect(() => {
+    try {
+      Promise.all(
+        data.map(([place, { api, url }]) =>
+          axios.get(url).then((res) => {
+            // do something with response
+            let placeCovidData = {};
+            if (api === "Worldometers") {
+              placeCovidData = res.data;
+            } else {
+              placeCovidData = res.data.filter((e) => e.province === place)[0];
+            }
+            markerData.current[`${place}`] = { api, data: placeCovidData };
+          })
+        )
+      );
+    } catch (e) {
+      console.log(e.message);
+    }
+    console.log(markerData.current);
+  }, []);
+
+  useEffect(() => {
+    const markers = markerRefs.current;
+    data.forEach(([place, placeData], idx) => {
+      const area = placeData.area;
+      let shouldShow = true;
+      if (area <= 2000000) {
+        shouldShow = zoom >= 3 ? true : false;
+      } else {
+        shouldShow = zoom >= 3 ? false : true;
+      }
+      markers[place].style.display = shouldShow ? "block" : "none";
+    });
+  }, [zoom]);
+
+  return (
+    <React.Fragment>
+      {data.map(([place, placeData], idx) => (
+        <Marker
+          key={idx}
+          latitude={placeData.latitude}
+          longitude={placeData.longitude}
+        >
+          <button
+            className="marker-btn"
+            id={`${place}-${idx}`}
+            onClick={(e) => {
+              e.preventDefault();
+              console.log(e.currentTarget.id);
+              console.log(placeData);
+              console.log(zoom);
+            }}
+            ref={(el) => (markerRefs.current[`${place}`] = el)}
+          >
+            <img src="/map-pin.svg" />
+          </button>
+        </Marker>
+      ))}
+    </React.Fragment>
+  );
+});
