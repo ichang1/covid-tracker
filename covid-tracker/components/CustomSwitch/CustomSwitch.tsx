975e8edae7a4b97eb4230f7c9d27d8d4280889a4
ichang1 <isaac.chang717@gmail.com>
2021-07-28 13:11:43 -0400

    added custom switch

---
+++ covid-tracker/components/CustomSwitch/CustomSwitch.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import Switch from "react-switch";
+import styles from "./CustomSwitch.module.css";
+
+interface CustomSwitchProps {
+  label: string;
+  state: boolean;
+  setState: React.Dispatch<React.SetStateAction<boolean>>;
+}
+
+const CustomSwitch = ({ label, state, setState }: CustomSwitchProps) => {
+  return (
+    <div className={styles["switch-container"]}>
+      <Switch
+        onChange={() => {
+          setState((state) => !state);
+        }}
+        checked={state}
+        checkedIcon={false}
+        uncheckedIcon={false}
+      />
+      <span className={styles["switch-label"]}>{label}</span>
+    </div>
+  );
+};
+
+export default CustomSwitch;
