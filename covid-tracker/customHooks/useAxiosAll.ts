a85b09e2201d34c33aa6c4c7f09bff03093160e8
ichang1 <isaac.chang717@gmail.com>
2021-08-01 22:13:01 -0400

    added useAxiosAll

---
+++ covid-tracker/customHooks/useAxiosAll.ts
@@ -0,0 +1,39 @@
+import { useQueries } from "react-query";
+import axios from "axios";
+axios.defaults.timeout = 15000;
+
+function fetchPromise(url: string) {
+  return async () => {
+    axios
+      .get(url)
+      .then((res) => res.data)
+      .catch((error) => error);
+  };
+}
+
+interface QueryOptions {
+  key: string | any[];
+  url: string;
+  enabled: boolean;
+}
+
+export default function useAxios(queryOptions: QueryOptions[]) {
+  const queryResults = useQueries(
+    queryOptions.map(({ key, url, enabled }) => ({
+      queryKey: key,
+      queryFn: fetchPromise(url),
+      enabled,
+    }))
+  );
+  const enabled = !queryOptions.some((queryOpt) => !queryOpt.enabled);
+  if (!enabled) {
+    return { data: {}, isLoading: false, isSuccess: true };
+  }
+  const isLoading = queryResults.some((query) => query.isLoading);
+  const data = queryResults.reduce(
+    (_data, query, idx) => ({ ..._data, [queryOptions[idx].url]: query.data }),
+    {}
+  );
+  const isSuccess = !queryResults.some((query) => !query.isSuccess);
+  return { data, isLoading, isSuccess };
+}
