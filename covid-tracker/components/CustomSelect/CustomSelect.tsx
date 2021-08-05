e3656ca05a50fa889ce62d0bc89693060bc8202e
ichang1 <isaac.chang717@gmail.com>
2021-08-04 21:49:39 -0400

    initial react select

---
+++ covid-tracker/components/CustomSelect/CustomSelect.tsx
@@ -0,0 +1,64 @@
+import React, { useState } from "react";
+import Select, { StylesConfig } from "react-select";
+import { randAlphaNum } from "../../utils/misc";
+import styles from "./CustomSelect.module.css";
+
+type Option = {
+  value: string;
+  label: string;
+};
+
+interface CustomSelectProps {
+  options: Option[];
+  setValue: (value: string) => void;
+}
+
+const customStyles: StylesConfig<Option, boolean> = {
+  menu: (provided, state) => ({
+    ...provided,
+    width: state.selectProps.width,
+    borderBottom: "1px dotted black",
+  }),
+  option: (provided, state) => ({
+    ...provided,
+    borderBottom: "1px dotted black",
+    color: state.isSelected ? "white" : "black",
+    padding: 5,
+  }),
+  control: (provided, { selectProps: { width } }) => ({
+    ...provided,
+    width: width,
+  }),
+  singleValue: (provided, state) => {
+    const opacity = state.isDisabled ? 0.5 : 1;
+    const transition = "opacity 300ms";
+
+    return { ...provided, opacity, transition };
+  },
+  noOptionsMessage: (provided, state) => {
+    return { ...provided };
+  },
+};
+
+export default function CustomSelect({ options, setValue }: CustomSelectProps) {
+  const [selectValue, setSelectValue] = useState<Option | null>(null);
+
+  const handleChange = (option: any) => {
+    const { value = null } = option;
+    setValue(value);
+    setSelectValue(option);
+  };
+
+  return (
+    <>
+      <Select
+        className={styles["custom-select"]}
+        options={options}
+        value={selectValue}
+        instanceId={`custom-select-${randAlphaNum(3)}`}
+        onChange={handleChange}
+        styles={customStyles}
+      />
+    </>
+  );
+}
