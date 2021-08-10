a024f4c2a447ceefeaa22dc20e244390bd2fcd20
ichang1 <isaac.chang717@gmail.com>
2021-08-10 14:00:45 -0400

    24 hour stale time added

--- covid-tracker/customHooks/useAxios.ts
+++ covid-tracker/customHooks/useAxios.ts
@@ -5,6 +5,8 @@ axios.defaults.timeout = 15000;
 function fetchPromise(url: string) {
   return () => axios.get(url).then((res) => res.data);
 }
+//24 hours
+const staleTime = 8.64 * 10 ** 7;
 export default function useAxios(
   key: string | any[],
   url: string,
@@ -14,6 +16,7 @@ export default function useAxios(
     queryKey: key,
     queryFn: fetchPromise(url),
     enabled,
+    staleTime,
   });
   if (!enabled) {
     return { data: {}, isLoading: false, isSuccess: true };
